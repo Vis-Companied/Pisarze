af5c5df8519455fd513ae81a4b1ccccf5543b536
Maciej Wiercioch <wierciochmaciej@endor.local>
2020-10-09 10:23:35 +0200

    09.10.2020

--- Vis.js
+++ Vis.js
@@ -1,12 +1,11 @@
+var i = 0;
 
-            function swapStyleSheet(sheet) {
-                document.getElementById("pagestyle").setAttribute("href", sheet);  
-            }
+function swapStyleSheet(sheet) {
+    document.getElementById("pagestyle").setAttribute("href", sheet);
+}
 
-            function initate() {
-                var style1 = document.getElementById("stylesheet1");
-                var style2 = document.getElementById("stylesheet2");
-
-                style1.onclick = document.swapStyleSheet("Vis.css");
-                style2.onclick = document.swapStyleSheet("VisN.css");
-            }
+function initate() {
+    let style = document.getElementsByName("switch");
+    i++;
+    style.onclick = swapStyleSheet(style[i % 2]['id']);
+}
