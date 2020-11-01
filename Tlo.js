ce280c5b3f46ca24d4ef53bdf811138058a88df5
MamFuu <Mamferek@gmail.com>
2020-11-01 13:37:10 +0100

    01.11.2020

--- Tlo.js
+++ Tlo.js
@@ -153,7 +153,7 @@
         if(!_this.active) return;
         ctx.beginPath();
         ctx.arc(_this.pos.x, _this.pos.y, _this.radius, 0, 2 * Math.PI, false);
-        ctx.fillStyle = 'rba(156,217,249,'+ _this.active+')';
+        ctx.fillStyle = 'rgba(156,217,249,'+ _this.active+')';
         ctx.fill();
     };
     }
